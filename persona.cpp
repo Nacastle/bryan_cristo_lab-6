91fe9a0b7a6b0c5c5f4bed7d03e3ad9c9cf1d67e
Cristobal Raul Romero Inestroza <crisonic123@hotmail.com>
2016-06-04 09:16:19 -0600

    clases

---
+++ persona.cpp
@@ -0,0 +1,22 @@
+#include "persona.h"
+#include <string>
+using std::string;
+persona::persona(string nombre,string,genero, string cabello,string ojos,string piel,bool fertil){
+
+}
+persona::persona(const Person&){
+
+}
+virtual persona::~persona(){
+
+}
+string persona::toString()const{
+
+}
+
+friend const persona operator+(const persona& hombre,const persona& mujer){
+
+}
+friend const persona operator*(const persona& hombre,const persona& mujer){
+
+}
--- persona.h
+++ persona.h
@@ -4,14 +4,18 @@ using std::string;
 
 class persona{
 	string nombre;
-  string genero;
-  string cabello;
+ 	string genero;
+  	string cabello;
 	string ojos;
-  string piel;
-  bool fertil;
-  public:
+  	string piel;
+  	bool fertil;
+ 	public:
 	persona(string, string, string, string, string, bool);
 	persona(const Person&);
 	virtual ~persona();
-	string toString()const;
+	string toString()const;	
 };
+
+friend const persona operator+(const persona&,const persona&);
+friend const persona operator*(const persona&,const persona&);
+
