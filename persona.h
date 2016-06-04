8826d27a6e87009ceac0572aa54125e2221fdd40
Bryan Alexis Mejia Fonseca <bryamejia@hotmail.com>
2016-06-04 08:56:32 -0500

    El .h de persona

---
+++ persona.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+using std::string;
+
+class persona{
+	string nombre;
+  string genero;
+  string cabello;
+	string ojos;
+  string piel;
+  bool fertil;
+  public:
+	persona(string, string, string, string, string, bool);
+	persona(const Person&);
+	virtual ~persona();
+	string toString()const;
+};
